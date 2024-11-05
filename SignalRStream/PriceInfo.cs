d58062c181fae04752cd7b36d65cd4e532718797
fardiis <fardis.bahry.94>
2024-11-05 15:16:07 -0500

    refactor

---
+++ SignalRStream/PriceInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalRStream
+{
+    public class PriceInfo
+    {
+        public string Symbol { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Symbol}: ${Price}";
+        }
+
+    }
+}
+
--- SignalRStream/TestHub.cs
+++ SignalRStream/TestHub.cs
@@ -72,16 +72,5 @@ namespace SignalRStream
 
         public IObservable<PriceInfo> PriceChange => priceServiceSubject.AsObservable();
     }
-
-    public class PriceInfo
-    {
-        public string Symbol { get; set; }
-        public double Price { get; set; }
-
-        public override string ToString()
-        {
-            return $"{Symbol}: ${Price}";
-        }
-    }
 }
 
